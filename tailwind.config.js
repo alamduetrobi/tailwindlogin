58d61befa2d5c0de6e4f52d19c8c58fb0db82944
Md. Rabiul Alam <alamrobiduet074001@gmail.com>
2022-01-09 20:41:17 +0600

    Tailwind login page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./layouts/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
